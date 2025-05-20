fe456a69bf380c9bf392712dacd7e444ede2f512
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-05-19 22:45:55 -0400

    Add BaseTimer class from which the SessionTimer and ClearingTimer inherit from

--- timers.py
+++ timers.py
@@ -2,7 +2,42 @@ from tkinter import Tk
 from config import SESSION_DURATION, CLEARING_DURATION
 
 
-class SessionTimer:
+class BaseTimer:
+    """
+    Base timer class.
+    """
+
+    def __init__(self, root: Tk, update_timer_display_callback):
+        self.root = root
+        self.update_timer_display_callback = update_timer_display_callback
+        self.after_id = None
+        self.duration = None
+        self.timer_type = None
+
+    def tick(self):
+        """
+        Decrement the current time by 1 second on each call. 
+        """
+        if self.duration < 0:
+            self.stop()
+
+        else:
+            self.minutes = self.duration // 60
+            self.seconds = self.duration % 60
+            self.update_timer_display_callback(
+                self.timer_type, self.minutes, self.seconds
+            )
+            self.duration -= 1
+            self.after_id = self.root.after(1000, self.tick)
+
+    def start(self):
+        self.tick()
+
+    def stop(self):
+        self.root.after_cancel(self.after_id)
+
+
+class SessionTimer(BaseTimer):
     """
     Class to manage the main session timer.
     """
@@ -14,34 +49,21 @@ class SessionTimer:
         disable_textbox_callback,
         times_up_callback,
     ):
-        self.root = root
-        self.after_id = None
-        self.duration = SESSION_DURATION
-        self.update_timer_display_callback = update_timer_display_callback
+        super().__init__(root, update_timer_display_callback)
         self.disable_textbox_callback = disable_textbox_callback
         self.times_up_callback = times_up_callback
+        self.duration = SESSION_DURATION
+        self.timer_type = "s"
 
     def tick(self):
         if self.duration < 0:
-            self.root.after_cancel(self.after_id)
             self.disable_textbox_callback()
             self.times_up_callback()
 
-        else:
-            self.minutes = self.duration // 60
-            self.seconds = self.duration % 60
-            self.update_timer_display_callback("s", self.minutes, self.seconds)
-            self.duration -= 1
-            self.after_id = self.root.after(1000, self.tick)
-
-    def start(self):
-        """
-        Start timer.
-        """
-        self.tick()
+        super().tick()
 
 
-class ClearingTimer:
+class ClearingTimer(BaseTimer):
     """
     Class to manage the clearing timer.
     """
@@ -52,29 +74,22 @@ class ClearingTimer:
         update_timer_display_callback,
         clearing_textbox_callback,
     ):
-        self.root = root
-        self.after_id = None
-        self.duration = CLEARING_DURATION
-        self.update_timer_display_callback = update_timer_display_callback
+        super().__init__(root, update_timer_display_callback)
         self.clearing_textbox_callback = clearing_textbox_callback
+        self.timer_type = "c"
 
     def tick(self):
         if self.duration < 0:
             self.clearing_textbox_callback()
-            self.root.after_cancel(self.after_id)
 
-        else:
-            self.minutes = self.duration // 60
-            self.seconds = self.duration % 60
-            self.update_timer_display_callback("c", self.minutes, self.seconds)
-            self.duration -= 1
-            self.after_id = self.root.after(1000, self.tick)
+        super().tick()
+
 
     def start(self, _event=None):
         """
-        Start timer. Cancels an existing timer if one is running.
+        Start clearing timer. Cancels an existing timer if one is running.
         """
         self.duration = CLEARING_DURATION
         if self.after_id:
             self.root.after_cancel(self.after_id)
-        self.tick()
+        super().tick()
